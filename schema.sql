a3477f964dcda3afceed3949fea7368fbf8aaca3
Zuhaib Amjad <umar0423@gmail.com>
2022-12-07 21:41:14 +0500

    Make changes in animals table
    And Create owners and species table

--- schema.sql
+++ schema.sql
@@ -7,5 +7,26 @@ CREATE TABLE animals (
     escape_attempts  INT          NOT NULL,
     neutered         BOOLEAN      NOT NULL,
     weight_kg        NUMERIC      NOT NULL,
-    species          TEXT         NOT NULL,   
+    species          TEXT         NOT NULL   
 );
+
+CREATE TABLE owners(
+    id           SERIAL PRIMARY KEY,
+    full_name    VARCHAR(50) NOT NULL,
+    age          INT NOT NULL
+);
+
+CREATE TABLE species(
+    id          SERIAL    PRIMARY KEY,
+    name         VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INT REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD COLUMN owner_id INT REFERENCES owners(id)
+ON DELETE CASCADE;
